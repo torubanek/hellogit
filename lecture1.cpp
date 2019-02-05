7b5efa7b79c154de1a1b9eaed14827cae4ce4757
torbuanek <torbuanek@users.noreply.github.com>
2019-02-05 11:04:45 -0400

    Added source file

---
+++ lecture1.cpp
@@ -0,0 +1,21 @@
+//Lab 1, a simple program on the first lecture
+#include <iostream>
+#inlcude <string>
+
+int main()
+{
+	string s0;
+	string s1="hello";
+	string s2=s1 + "	world";
+	
+	cout<<"s0="<<s0<<endl;
+	cout<<"s1="<<s1<<endl;
+	cout<<"The 4th character of s1 is "<<5|[3]<<endl;
+	cout<<"s1 has "<<s1.size()<<"characters<<endl;
+	if (s1=="hello")
+		cout<<"Hi there!"<<endl;
+	if (s2!=="world")
+		cout<<"Out of this world!<<endl;
+	system("pause");
+	return 0;
+}
